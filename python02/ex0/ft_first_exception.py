9997a1d2c82f68c056f2d43655b24cc066ad9fce
Triano83 <wisky@DESKTOP-ESB68TP.localdomain>
2026-03-05 10:31:41 +0100

    Prueba 1

---
+++ python02/ex0/ft_first_exception.py
@@ -0,0 +1,25 @@
+def check_temperature(temp_str):
+    try:
+        temp = int(temp_str)
+        if temp > 40:
+            print(f"Error: {temp}°C is too hot for plants (max 40°C)\n")
+            return None
+        elif temp < 0:
+            print(f"Error: {temp}°C is too cold for plants (min 0°C)\n")
+            return None
+        return temp
+    except:
+        print(f"Error: '{temp_str}' is not a valid number\n")
+        return None
+def test_temperature_input():
+    temp_list = ["25","abc","100","-50"]
+    print(f"=== Garden Temperature Checker ===\n")
+    for temp in temp_list:
+        print(f"Testing temperature: {temp}")
+        temp_int = check_temperature(temp)
+        if temp_int is not None and temp_int >= 0 and temp_int <= 40:
+            print(f"Temperature {temp_int}°C is perfect for plants!\n")
+    print(f"All test completed - program dind`t crash!!")
+
+if __name__ == "__main__":
+    test_temperature_input()
---
+++ python02/ex1/ft_different_errors.py
@@ -0,0 +1,41 @@
+def garden_operations():
+    try:
+        print("Testing ValueError...")
+        number = int("abc")
+    except ValueError as e:
+        print(f"Caught ValueError: {e}\n")
+    
+    try:
+        print("Testing ZeroDivisionError...")
+        division = 10 / 0
+    except ZeroDivisionError as e:
+        print(f"Caught ZeroDivisionError: {e}\n")
+    
+    try:
+        print("Testing FileNotFoundError...")
+        archive = open("test.txt")
+    except FileNotFoundError as e:
+        print(f"Caught FileNotFoundError: {e}\n")
+    
+    try:
+        print("Testing KeyError...")
+        errorDict = {"Rose":1,"SunFlower":2}
+        print(f"{errorDict['Missing']}")
+    except KeyError as e:
+        print(f"Caught KeyError: {e}\n")
+    
+
+def test_error_types():
+    print(" === Garden Error Types Demo ===\n")
+    garden_operations()
+
+    try:
+        print("Testing multiple errors together...")
+        int("esto_no_es_un_numero")
+        value = 10 / 0
+    except (ValueError, ZeroDivisionError) as e:
+        print(f"Caught an error, but program continues!: {e}\n")
+    print("All error types tested succesfully!")
+
+if __name__ == "__main__":
+    test_error_types()
